5964eb67867f91d949940931d846a6d5508c49f9
R0nio <dimabirucob@gmail.com>
2025-09-26 09:31:07 +0500

    Сайт начало

---
+++ resources/views/report/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+    <header>
+        <div>
+            <h1>Нарушений<span class="logo_red">.Нет</span></h1>
+            <div class="headere_login">
+                <p>Носова Ольга Петровна</p>
+                <button class="">></button>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
